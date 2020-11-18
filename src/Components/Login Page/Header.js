bd8eacf2fc43d47813af7b20887b637f806198d8
CharithNiroshan <charithniroshan44@gmail.com>
2020-11-18 07:34:46 +0530

    Create the react app

---
+++ src/Components/Login Page/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Hello Clever Programmers ! Let's Start the React Theory Tutorial</h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Components/Login Page/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import "./Login.css";
+
+function Login(props) {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default Login;
